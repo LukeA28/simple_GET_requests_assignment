8cbaeb61b9efc71a09db2ae77da9f58955a33a2e
Luke Allen <lmallen040@gmail.com>
2019-09-05 20:31:40 -0500

    completed script to display images

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 'use strict';
 
 function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random/3')
+  fetch(`https://dog.ceo/api/breeds/image/random/${$('.value').val()}`)
     .then(response => response.json())
     .then(responseJson => 
-      displayResults(responseJson))
+      displayResults(responseJson.message))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 function displayResults(responseJson) {
-  console.log(responseJson);
-  //replace the existing image with the new one
-  for (let i = 0; i < $('.value').val(); i++) {
-    $('.results').append(
-        `<img src="${responseJson.message[i]}" class="results-img">`
-    );
+    console.log(responseJson);
+    //push the image url into an array
+    for (let i = 0; i < $('.value').val(); i++) {
+        $('.results').append(`<img src="${responseJson[i]}" alt="random_dog">`)
+    }
+    //display the results section
+    $('.results').removeClass('hidden');
   }
-  //display the results section
-  $('.results').removeClass('hidden');
-}
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
+    $('.results').empty();
+    $('.results').text('Look at this dog!');
     getDogImage();
   });
 }
